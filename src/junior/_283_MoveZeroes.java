dc12d553ec5e03ac94b9447551ce242c788c019e
fengxxc <fengxxc@gmail.com>
2021-08-04 12:13:47 +0800

    doc(junior): 补充注释

--- src/junior/_283_MoveZeroes.java
+++ src/junior/_283_MoveZeroes.java
@@ -3,6 +3,10 @@ package junior;
 import java.util.Arrays;
 
 /**
+ * @author fengxxc
+ *
+ * 移动零
+ *
  * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
  *
  * 示例:
@@ -13,6 +17,9 @@ import java.util.Arrays;
  *
  * 必须在原数组上操作，不能拷贝额外的数组。
  * 尽量减少操作次数。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/move-zeroes
  */
 public class _283_MoveZeroes {
     public static void moveZeroes(int[] nums) {
