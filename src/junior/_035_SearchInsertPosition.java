fce4974bb29f8b8b0389b880723064cffb58e0cb
fengxxc <fengxxc@gmail.com>
2021-08-11 15:06:58 +0800

    feat(junior): 35.搜索插入位置

---
+++ src/junior/_035_SearchInsertPosition.java
@@ -0,0 +1,71 @@
+package junior;
+
+/**
+ * @author chenfeng
+ *
+ * 搜索插入位置
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: nums = [1,3,5,6], target = 2
+ * 输出: 1
+ * 示例 3:
+ *
+ * 输入: nums = [1,3,5,6], target = 7
+ * 输出: 4
+ * 示例 4:
+ *
+ * 输入: nums = [1,3,5,6], target = 0
+ * 输出: 0
+ * 示例 5:
+ *
+ * 输入: nums = [1], target = 0
+ * 输出: 0
+ *  
+ *
+ * 提示:
+ *
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 为无重复元素的升序排列数组
+ * -10^4 <= target <= 10^4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-insert-position
+ */
+public class _035_SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = (r - l) / 2 + l;
+            if (target <= nums[m]) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        // int[] nums = {1};
+
+        // int target = 5;
+        int target = 2;
+        // int target = 7;
+        // int target = 0;
+        final int i = new _035_SearchInsertPosition().searchInsert(nums, target);
+        System.out.println(i);
+    }
+}
