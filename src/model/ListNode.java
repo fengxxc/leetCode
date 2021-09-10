1b6b656cab211c85cb97918ee6737625cfb4cf7b
fengxxc <fengxxc@gmail.com>
2021-09-10 12:04:47 +0800

    feat(model): 链表类，添加构造方法

--- src/model/ListNode.java
+++ src/model/ListNode.java
@@ -3,11 +3,13 @@ package model;
 public class ListNode {
 	public int val;
 	public ListNode next;
-	public ListNode(int x) {
-		val = x;
-		next = null;
-	}
-	 
+
+	public ListNode() {}
+
+	public ListNode(int x) { val = x; }
+
+	public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
 	public ListNode setNextVal(int nextVal) {
 		this.next = new ListNode(nextVal);
 		return this.next;
